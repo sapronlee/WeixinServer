9038ed1535ec026f6d2a946e098dfffa2f326ad6
joseen <qs.joseen@gmail.com>
2013-07-16 14:11:54 +0800

    修改gem

--- app/models/web_config.rb
+++ app/models/web_config.rb
@@ -1,10 +1,10 @@
-# enconding : utf-8
+# encoding: UTF-8
 
 class WebConfig
   include Mongoid::Document
   include Mongoid::Timestamps
   include Mongoid::AppSettings
-  
+
   setting :app_name,                  default: '微信公众账号后台系统'
   setting :default_text_message,      default: '亲，我不知道你写的是什么！'
   setting :default_voice_message,     default: '亲，我不知道你说的是什么！'
@@ -12,5 +12,5 @@ class WebConfig
   setting :default_video_message,     default: '亲，这段视频看不懂哦！'
   setting :default_link_message,      default: '亲，这个链接看不懂！'
   setting :default_location_message,  default: '亲，找不到这个位置的内容哦！'
-  
+
 end
