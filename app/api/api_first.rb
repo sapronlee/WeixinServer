1ec36b603bd4cf5653bd3ece21220a640e24de33
joseen <qs.joseen@gmail.com>
2013-07-22 09:59:15 +0800

    api: create new accounts

--- app/api/api_first.rb
+++ app/api/api_first.rb
@@ -6,23 +6,31 @@ class API_First < Grape::API
   resource :accounts do
     desc "create account"
     params do
-      requires :name,    type: String
-      requires :en_name, type: String
-      requires :desc,    type: String
-      requires :avatar,  type: Object
-      requires :qr_code, type: Object
+      requires :identifier,   type: String
+      requires :name,         type: String
+      requires :en_name,      type: String
+      requires :desc,         type: String
+      requires :avatar,       type: Object
+      requires :qr_code,      type: Object
     end
     post do
-      account = Account.new name: params[:name],
-                         en_name: params[:en_name],
-                            desc: params[:desc],
-                          avatar: params[:avatar],
-                         qr_code: params[:qr_code]
-      if account.save
-        { result: { status: "ok" }, infos: { token: "#{account.token}" } }
-      else
-        binding.pry
-        { result: { status: "no" } }
+      begin
+        company = Company.where(identifier: params[:identifier]).first
+        if company
+          account = company.accounts.new  name: params[:name],
+                                       en_name: params[:en_name],
+                                          desc: params[:desc],
+                                        avatar: params[:avatar],
+                                       qr_code: params[:qr_code]
+
+          if account.save
+            { result: { account: { token: "#{account.token}" } } }
+          else
+            error!({ result: { error: "#{account.errors.full_messages.join(',')}" } }, 400)
+          end
+        end
+      rescue
+        error!({ reuslt: { error: "Invalid request information" } }, 400)
       end
     end
   end
